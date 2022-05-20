a97ab0d02344be2ad7d2890e3806601e8326cb1e
eagersolve <ekstaz1993@yandex.ru>
2022-05-20 14:18:01 +0400

    -25.50 Все работает

---
+++ app/addons/sd_collections/schemas/menu/menu.post.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+*                                                                          *
+*   (c) Simtech Development Ltd.                                           *
+*                                                                          *
+* This  is  commercial  software,  only  users  who have purchased a valid *
+* license  and  accept  to the terms of the  License Agreement can install *
+* and use this program.                                                    *
+*                                                                          *
+***************************************************************************/
+
+$schema['central']['products']['items']['sd_collections.collections'] = [
+    'href' => 'products.manage_collections',
+    'position' => 250,
+];
+
+return $schema;
